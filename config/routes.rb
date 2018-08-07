5e7be398ba6483deb0fcf166d17d31682f6cfe73
edotensei <edotensei19@gmail.com>
2018-08-07 12:29:59 +0700

    fix _task and routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,15 @@ Rails.application.routes.draw do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
   end
-
-  resources :users, only: %i[show update edit current] do
+  resources :users, only: %i[show update edit] do
     collection do
       get :profile
     end
   end
-  resources :tasks, only: %i[new create update destroy edit] do
+  resources :tasks do
     member do
-      put 'complete'
-      put 'take'
+      put :complete
+      put :take
     end
   end
 end
